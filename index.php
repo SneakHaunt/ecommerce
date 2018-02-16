88901624f9092100fff66d13018b79fa4134ba15
Osni-coj <osni.coj@hotmail.com>
2018-02-15 23:25:37 -0200

    Criamos a classe Page

--- index.php
+++ index.php
@@ -2,18 +2,29 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim(); //$app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
+	/*
+    Isso foi usado só para testar
 	$sql = new Hcode\DB\Sql();
-
 	$results = $sql->select("SELECT * FROM tb_users");
-
 	echo json_encode($results);
-	
+	*/
+
+	$page = new Page(); //Chama __construct que adicionara o header.
+
+	$page->setTpl("index"); //Adiciona Hello.
+	/*
+	Termina a execução, o PHP limpa a memória do código, chamando o __destruct que irá incluir o footer.
+	*/
+
+
 });
 
 $app->run();
---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,90 @@
+<?php 
+/*
+
+array_merge - The array_merge() function merges one or more arrays into one array.
+
+Definition and Usage
+The array_merge() function merges one or more arrays into one array.
+
+Tip: You can assign one array to the function, or as many as you like.
+
+Note: If two or more array elements have the same key, the last one overrides the others.
+
+Se executar este código:
+
+$a1=array("a"=>"red","b"=>"green");
+$a2=array("c"=>"blue","b"=>"yellow");
+print_r(array_merge($a1,$a2));
+
+O retorno será:
+Array ( [a] => red [b] => yellow [c] => blue )
+
+É sempre o valor do último array que persiste caso seja encontrado chaves iguais. O último sempre sobreescreve os anteriores.
+
+*/
+
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Page{
+
+	private $tpl;
+	private $options =[];
+	private $defaults = [
+		"data" => []
+	];
+
+	public function __construct($opts = array()){
+
+		$this->options = array_merge($this->defaults, $opts);
+
+		$config = array(
+				"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . "/views/",
+				"cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
+				"debug"         => false // set to false to improve the speed
+				 );
+
+		Tpl::configure( $config );
+
+		/*
+			Usa-se this, pois, é mais interessante ele ser um atributo da classe, assim,  tenho acesso ao tpl em outros métodos.
+		*/
+		$this->tpl = new Tpl;
+
+		$this->setData($this->options["data"]);
+
+		$this->tpl->draw("header");
+
+	}
+
+	private function setData($data = array())
+	{
+		foreach ($data as $key => $value) {
+			$this->tpl->assign($key, $value);
+		}
+	}
+
+
+	public function setTpl($name, $data = array(), $returnHTML = false)
+	{ //Último método a ser executado.
+
+		$this->setData($data);
+
+		/*
+		$tpl->draw( 'demo', TRUE ); return template in string or 
+		$tpl->draw( $tpl_name ); no caso quando não tem nada é FALSE...  echo the template
+		*/
+		return $this->tpl->draw($name, $returnHTML);
+
+	}
+
+	public function __destruct() //Último método a ser executado.
+	{	
+		$this->tpl->draw("footer");
+	}
+
+}
+
+?>
