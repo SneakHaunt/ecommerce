52dd8fab003907f03f186c45b72a8845ee6836ea
Osni-coj <osni.coj@hotmail.com>
2018-02-18 20:39:27 -0300

    Criando PageAdmin e tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim(); //$app = new \Slim\Slim();
 
@@ -19,13 +20,26 @@ $app->get('/', function() {
 
 	$page = new Page(); //Chama __construct que adicionara o header.
 
-	$page->setTpl("index"); //Adiciona Hello.
+	$page->setTpl("index"); //Adiciona Conteudo.
 	/*
 	Termina a execução, o PHP limpa a memória do código, chamando o __destruct que irá incluir o footer.
 	*/
 
 
 });
+
+$app->get('/admin', function() {
+	
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+	
+});
+
+
+
+
+
 
 $app->run();
 
